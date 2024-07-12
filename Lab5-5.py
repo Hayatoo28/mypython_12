9baca67d419d589caae923be8ed734b0bb2491bc
Hayatoo28 <peerapat.sinlatham2812@gmail.com>
2024-07-12 14:17:00 +0700

    update

--- Lab5-5.py
+++ Lab5-5.py
@@ -5,17 +5,17 @@ def beforMidterm():
 a = int(input("คะแนนเก็บ : "))
 if score > 20:
     print("คะแนนเก็บไม่เกิน 20")
-
+else:
     
 b = int(input("คะแนนจิตพิสัย : "))
 if score > 10:
     print("คะแนนจิตพิสัยไม่เกิน 10")
-
+else:
     
 c = int(input("คะแนนกลางภาคไม่เกิน : "))
 if score > 30:
     print("คะแนนกลางภาคไม่เกิน 30")
-
+else:
 
     
     print("คะแนนรวม %2f" %beforMidterm())
